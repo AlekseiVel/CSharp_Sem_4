b49a31994d41658a624427347914db0adc0703b9
«AlekseiVel» <velmezov.au@gmail.com>
2022-08-28 16:36:48 +0300

    Ex28

---
+++ Ex28/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, котоая принимает на вход число
+// N и выдает произведение чисел от 1 до N
+
+int GetFact(int a)
+{
+    int fact = 1;
+
+    for(int i = 1; i <= a; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
+
+Console.WriteLine("Введите ваше чило?");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение чисел от 1 до {a} равна: ");
+Console.Write(GetFact(a));
