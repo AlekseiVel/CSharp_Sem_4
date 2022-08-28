07d1918f6a519aae349735d36506fe2ae166c306
«AlekseiVel» <velmezov.au@gmail.com>
2022-08-28 19:58:23 +0300

    Ex29Home

---
+++ Ex29Home/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая задает массив из 8 элементов и 
+// выводит их на экран 
+
+Console.Clear();
+
+int[] RandomNumbersArray(int size)
+{
+    int[] numbersArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Вводите элементы массива: ");
+        numbersArray[i] = int.Parse(Console.ReadLine()!);
+    }
+    return numbersArray;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
+
+Console.WriteLine("Введите размер массива?");
+int size = int.Parse(Console.ReadLine()!);
+int[] array = RandomNumbersArray(size);
+PrintArray(array);
