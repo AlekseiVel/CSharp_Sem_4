9b52fd2d9e9e88f42cbdd6427c8c718d8faf6782
«AlekseiVel» <velmezov.au@gmail.com>
2022-08-28 11:13:40 +0300

    Ex24

---
+++ Ex24/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает число (А)
+// и выдает сумму чисел от 1 до А.
+
+int GetSum1ToA(int a)
+{
+    int sum = 0;
+
+    for(int i = 1; i <= a; i++)
+    {
+        //sum = sum + i;
+        sum += i;
+    }
+    return sum;
+}
+
+//Console.WriteLine("Введите ваше чило?");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine($"Сумма чисел от 1 до {a} равна: ");
+//Console.Write(GetSum1ToA(a));
+
+// Второй вариант
+
+int GetSum(int a)
+{
+    return a * (a + 1) / 2;
+}
+
+Console.WriteLine(GetSum(8));
