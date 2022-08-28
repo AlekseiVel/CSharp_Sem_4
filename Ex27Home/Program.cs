9f7d2a45525d6d0eef0aee1343925fcce7096c4e
«AlekseiVel» <velmezov.au@gmail.com>
2022-08-28 19:44:00 +0300

    ex27h

---
+++ Ex27Home/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму
+// цифр в числе.
+
+Console.Clear();
+
+int SumOfDigits(int a)
+{
+    int result = 0;
+    for (int i = 0; a > 0; i++)
+    {
+        int help = a % 10;
+        result = result + help;
+        a /= 10; 
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumOfDigits(a));
