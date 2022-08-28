23db998b40430ac0e213b46e145652151774a27f
«AlekseiVel» <velmezov.au@gmail.com>
2022-08-28 16:18:34 +0300

    Ex26

---
+++ Ex26/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая принимает на вход
+// число и выдает количество цифр в числе
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+Console.Write("Введите число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine()!);
+string numberText = Convert.ToString(anyNumber);
+int count = 0;
+while (anyNumber > 0)
+{
+    //anyNumber = anyNumber / 10;
+    anyNumber /= 10;
+    count++;
+}
+
+//Console.WriteLine("Количество цифр в числе: " + count);
+//Console.WriteLine("Количество цифр в числе: " + numberText.Length);
+
+//Второй вариант:
+
+Console.Clear();
+
+int DigitsCount(int number)
+{
+    if(number == 0)
+    {
+        return 1;
+    }
+    int count = 0;
+    for(; number > 0; count++)
+    {
+        number /= 10;
+    }
+    return count;
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(DigitsCount(number));
